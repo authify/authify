308c8978539527da01eb5be34ba8da5ec937e32d
Jonathan Gnagy <jonathan@therubyist.org>
2026-08-26 10:21:37 -0700

    Support client_secret_basic authentication on token endpoint (#200)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Authify.MixProject do
   def project do
     [
       app: :authify,
-      version: "0.19.2",
+      version: "0.19.3",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
